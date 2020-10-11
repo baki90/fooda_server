a432e61bc29d7a8db37ec3ed91d87a3136def1ff
Ubuntu <ubuntu@ip-172-31-25-39.us-east-2.compute.internal>
2020-10-11 12:45:07 +0000

    음식 영양소 측정

--- diet.py
+++ diet.py
@@ -1,14 +1,32 @@
 #about user
 from flask_restful import Resource, Api
 from flask import request
-#from yolo import detect as Detector
+import db
+
+#food detector
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import yolo.detect as Detector
+
+def nutrient(foodname):
+    cursor = db.connect()
+    sql = "select * from food where food_name='%s'"%(foodname)
+    print(sql)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if len(result) >0:
+        return result
+    else:
+        return 'fail'
 
 class AnalyzeDiet(Resource):
     def post(self): #if using post method
         value = request.form['token']
         foodImage = request.files['uploaded_file'] #download file from application
         foodImage.save('food.png')
-
-        #foodname = Detector.foodDetect("../food.png")
-        print(foodname)
+       
+        foodname = Detector.foodDetect("../fooda-server/food.png")
+        for fn in foodname:
+            print(nutrient(fn))
         return {"result": "success"} #return json
