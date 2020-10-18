dab938a568092cb04d9ae283da5ca64600c7f658
Ubuntu <ubuntu@ip-172-31-25-39.us-east-2.compute.internal>
2020-10-18 15:59:08 +0900

    레시피 크롤링

--- foodCrawling.py
+++ foodCrawling.py
@@ -7,13 +7,35 @@ def foodCrawling(foodname):
     url = "https://www.10000recipe.com" #만개의 레시피
     requrl = url +"/recipe/list.html?q=" + urllib.parse.quote(foodname) #한글 인코딩
     print(requrl)
-    req = urllib.request.Request(requrl)
     sourcecode = urllib.request.urlopen(requrl).read()
     soup = BeautifulSoup(sourcecode, "html.parser")
     link = soup.find_all(class_='common_sp_link')[0]['href'] #최 앞에 있는 레시피
-    print(link)
 
-    requrl
+    requrl = url + link #해당 식단의 레시피 페이지를 크롤링한다.
+    req = urllib.request.Request(requrl)
+    sourcecode = urllib.request.urlopen(requrl).read()
+    soup = BeautifulSoup(sourcecode, "html.parser")
+    
+    source = []
+    step = []
 
+    res = soup.find('div', 'view2_summary st3')
+    res = res.find('h3')
+    title= res.get_text()
+    res = soup.find('div','view2_summary_info')
+    level = res.get_text().replace('\n','')
+    res = soup.find('div', 'ready_ingre3')
+    try:
+        for n in res.find_all('ul'):
+            for jae in n.find_all('li'):
+                source.append(jae.get_text().replace('\n','').replace(' ',''))
+    except (AttributeError):
+        return
 
-foodCrawling("샌드위치")
+    res = soup.find('div', 'view_step')
+    i =0
+    for n in res.find_all('div', 'view_step_cont'):
+        i += 1
+        step.append(str(i) +'. ' + n.get_text().replace('\n',' '))
+    
+    return title, level, source, step
